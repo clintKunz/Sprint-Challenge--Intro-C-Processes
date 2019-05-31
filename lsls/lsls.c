f518427b2e50b06aad302735b127f6d5944c5d19
Clint Kunz <clintkunz@gmail.com>
2019-05-31 10:11:26 -0600

    prints file size for each file in directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,6 +4,7 @@
 #include <libgen.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -13,7 +14,7 @@ int main(int argc, char **argv)
   // Parse command line
   //printf("%s\n", argv[0]);
   //printf("%s\n", argv[1]);
-  
+
   // Open directory
   DIR *dir;
   struct dirent *dp;
@@ -25,10 +26,13 @@ int main(int argc, char **argv)
   // Repeatly read and print entries
   while ((dp = readdir (dir)) != NULL) {
     // print file size
-
+    struct stat st;
+    stat(dp->d_name, &st);
+    int size = st.st_size;
+    printf("%d ", size);
     // print file name
-    printf("%s\n", dp->d_name);
-  }
+    printf(" %s\n", dp->d_name);
+  } 
   // Close directory
   closedir(dir);
 
